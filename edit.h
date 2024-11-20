41f1306f19b7d41a67ecc5d6ccfeab9f91c7e42a
aruncyriac <aruncyriac01@gmail.com>
2024-11-20 20:49:56 +0530

    edit.h

--- edit.h
+++ edit.h
@@ -2,10 +2,9 @@
 #define EDIT_H
 #include"common.h"
 
-Status read_validate(char *argv[],int argc,char *tag,char tags [][5]);
-  
-Status do_editing(File *file,char *tag);
-Status is_found(File *file);
-int little_to_big(int x);
+Status read_validate(char *argv[],int argc,char *tag,char tags [][5]); //function to validate the CLA's 
+Status do_editing(File *file,char *tag); //function to editing
+Status is_found(File *file); //function to find tag
+Status copy_remaining(File *file);//function to copy the remaining data to file
 
 #endif
